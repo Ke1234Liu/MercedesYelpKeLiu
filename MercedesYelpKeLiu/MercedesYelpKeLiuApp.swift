e43319718410c25a9539a3570000e78efad3bda8
Ke Liu <ke.umb.cs@gmail.com>
2023-01-25 21:40:33 -0500

    Initial Commit

---
+++ MercedesYelpKeLiu/MercedesYelpKeLiuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercedesYelpKeLiuApp.swift
+//  MercedesYelpKeLiu
+//
+//  Created by Ke Liu on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MercedesYelpKeLiuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
